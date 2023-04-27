c6256237ed0dd4db1e394b42ae5833b29468bd73
lobatt <lob4tt@gmail.com>
2023-04-27 11:15:47 -0400

    using chat completion api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessages([...messages, { type: 'user', content: input }]);
+
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: "gpt-3.5-turbo",
+          messages: [...messages, { role: 'user', content: input }],
+          max_tokens: 150,
+          n: 1,
+          stop: null,
+          temperature: 1,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+          },
+        },
+      );
+
+      const aiResponse = response.data.choices[0].message.content.trim();
+      setMessages([...messages, { role: 'assistant', content: aiResponse }]);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+
+    setInput('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGPT Frontend</h1>
+      <div className="chat-container">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.type}`}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
